643a9d44ebd0885c0bcc749ae04cb4a7aeea6fd0
Phil <birkal@outlook.com>
2015-10-06 13:11:53 -0700

    merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 
-module.exports = {
+var common = {
 	entry: path.resolve(ROOT_PATH, 'app'),
 	output: {
 		path: path.resolve(ROOT_PATH, 'app'),
@@ -19,16 +21,24 @@ module.exports = {
 			}
 		]
 	},
-	devServer: {
-		historyApiFallback: true,
-		hot: true,
-		inline: true,
-		progress: true
-	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
 		new HtmlwebpackPlugin({
 			title: 'Kanban app'
 		})
 	]
 };
+
+if (TARGET === 'start' || !TARGET) {
+	module.exports = merge(common, {
+		devtool: 'eval-source-map',
+		devServer: {
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+		]
+	});	
+}
