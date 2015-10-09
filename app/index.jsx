a74ad33845feb135867333b7c37855e097fe78a0
Phil <birkal@outlook.com>
2015-10-09 00:06:45 -0700

    persist data

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,8 @@
 import 'array.prototype.findindex';
 import './main.css';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 
 import React from 'react';
 import App from './components/App.jsx';
@@ -7,6 +10,8 @@ import App from './components/App.jsx';
 main();
 
 function main() {
+	persist(alt, storage, 'app');
+
 	const app = document.createElement('div');
 
 	document.body.appendChild(app);
